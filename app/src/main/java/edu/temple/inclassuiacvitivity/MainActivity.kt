b791052f11a6e46ea17b74298eebc20cc646fdaf
Aaron <aarocka@protonmail.com>
2023-02-07 14:57:13 -0500

    added random array and spinner selector thingy

--- app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
@@ -2,8 +2,12 @@ package edu.temple.inclassuiacvitivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.*
+import android.widget.AdapterView.OnItemSelectedListener
+import kotlin.random.Random
+import kotlin.random.nextInt
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,13 +19,33 @@ class MainActivity : AppCompatActivity() {
 
         /* Step 1: Populate this array */
         //val numberArray = Array Of Numbers
+        val numberArray = IntArray(100)
+        for (i in 0..99){
+            numberArray[i]= Random.nextInt(0,100)
+        }
+
+        for (i in numberArray){
+            Log.d("array output", numberArray[i].toString())
+        }
+
 
         /* Step 2: Create adapter to display items from array in Spinner */
         //spinner.adapter = ArrayAdapter...
-
+        spinner.adapter = ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, numberArray.toList())
 
         // Step 3: Change TextView's text size to the number selected in the Spinner */
         //spinner.onItemSelectedListener = object: ...
+        spinner.onItemSelectedListener = object: OnItemSelectedListener {
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                if (p0 != null) {
+                    displayTextView.text=p0.selectedItem.toString()
+                }
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                displayTextView.text="lol"
+            }
+        }
 
     }
 }
